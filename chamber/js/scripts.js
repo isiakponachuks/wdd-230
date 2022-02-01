21bb96b2c3c24a7b3be46258965c22db23ec1085
isiakponachuks <isi21001@byui.edu>
2022-02-01 11:52:31 +0100

    favicon

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -16,12 +16,12 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+/* const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
 // long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`; */
 
 
