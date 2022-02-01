a2950814e49323e20039296d9da1558a4faaba99
isiakponachuks <isi21001@byui.edu>
2022-02-01 12:21:29 +0100

    edit

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -15,13 +15,12 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-
-/* const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
 // long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`; */
+
 
 
