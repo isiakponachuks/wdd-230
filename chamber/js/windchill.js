236b05ce442aca430242d5ddbbd85c7ba7ec61ab
isiakponachuks <isi21001@byui.edu>
2022-02-14 03:38:13 +0100

    comit

---
+++ chamber/js/windchill.js
@@ -0,0 +1,17 @@
+let = parseFloat.document.querySelector('#t').textContent;
+let = parseFloat.document.querySelector('#s').textContent;
+let windchill = '';
+
+if (t <=50 && > 3){
+windchill = windchill(t,s);
+windchill = `${windchill} &#176;F`
+}
+
+else {
+    windchill ='N/A';
+}
+
+document.querySelector('n/a').textContent = windchill(temp,speed){
+    return math.pow (0,16)
+}
+
