ff1f30d2ba59640921687a607e779352402d19d4
isiakponachuks <isi21001@byui.edu>
2022-03-12 15:02:27 +0100

    commit

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,13 +1,11 @@
 
-
- 
-/*let t = document.querySelector('#t').textContent;
+let t = document.querySelector('#t').textContent;
 let s = document.querySelector('#s').textContent;
 let windchill = '';
 
 if (t <=50 && s > 3){
 windchill = windChill(t,s);
-windchill = `${windchill}&#176;F`;
+
 }
 
 else {
@@ -18,10 +16,8 @@ document.querySelector('#w').innerHTML = windchill;
 function windChill(temp,speed){ 
     return Math.pow(s,0.16) 
 }
-/*
 
-
-/*const temp = 60;
+const temp = 60;
 
 const wSpeed =30;
 
@@ -32,25 +28,5 @@ let windChill = Math.round(windChill);
 document.getElementById('windChill').innerHTML = windChill;
 
 
-*/
-
-let windChill = Math.round(windChill);
-
-document.getElementById('windChill').innerHTML = windChill;
-
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Nigeria,lagos&APPID=8dc8da65a8bf4742511f5faaf0ae532f";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.querySelector('#current-temp').textContent = jsObject.main.temp;
-  });
-
-const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-const desc = jsObject.weather[0].description;
 
 
-document.querySelector('#icon-src').textContent = iconsrc;
-document.querySelector('#weathericon').setAttribute('src', iconsrc);
-document.querySelector('#weathericon').setAttribute('alt', desc);
-document.querySelector('figcaption').textContent = desc;
