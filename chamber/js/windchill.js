01d4501f10db2ca141ff13de284040743f3efef9
isiakponachuks <isi21001@byui.edu>
2022-03-16 08:11:56 +0100

    comit

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,34 +1,26 @@
+let t = parseFloat(document.querySelector("#t").textContent);
+let s = parseFloat(document.querySelector("#s").textContent);
+let windchill = "";
 
-let t = document.querySelector('#t').textContent;
-let s = document.querySelector('#s').textContent;
-let windchill = '';
-
-if (t <=50 && s > 3){
-windchill = windChill(t,s);
-
-}
-
+if (t <= 50 && s > 3) {
+windchill = windChill(t, s);
+windchill = `${windchill} &#176;F`;
+} 
 else {
-    windchill ='N/A';
+windchill = "N/A";}
+
+document.querySelector("#w").innerHTML = windchill;
+
+function windChill(temp, speed) {
+return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);}
+
+const t = parseInt(document.querySelector("#temp").textContent);
+const s = parseInt(document.getElementById("speed").textContent);
+
+if(t <= 50 && s > 3) {
+    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
+    document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
 }
-
-document.getElementById('windChill').innerHTML = windChill;
-document.querySelector('#w').innerHTML = windchill;
-
-function windChill(temp,speed){ 
-    return Math.pow(s,0.16) 
-}
-
-const temp = 60;
-
-const wSpeed =30;
-
-let windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-
-let windChill = Math.round(windChill);
-
-document.getElementById('windChill').innerHTML = windChill;
-
-
-
-
+else {
+        document.querySelector("#windchill").innerHTML = "N/A"
+    }
