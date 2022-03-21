8ea5616c69415e8668d6a49e2b996f4b30b7d71e
isiakponachuks <isi21001@byui.edu>
2022-03-21 22:50:39 +0100

    comit

--- chamber/js/spot.js
+++ chamber/js/spot.js
@@ -1,42 +1,37 @@
-
-const requestURL = 'https://isiakponachuks.github.io/wdd-230/chamber/data/data.json';
+const requestURL = 'https://isiakponachuks.github.io/wdd-230/chamber/data/spotlight.json';
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-  console.table(jsonObject);  // temporary checking for valid response and data parsing
-  const directory = jsonObject['directory'];
-    
-let companiesFiltered = directory.filter(i => i.membership == 'gold' || i.membership == 'silver')
-.sort(function() {return Math.random() - 0.5});
-        
-        
-    const companies = jsonObject['directory'];
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
 
-    for (let i = 11; i < directory.length; i++ ) {
+    const directory = jsonObject['directory'];
+    for (let i = 0; i < directory.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let image = document.createElement('img');
-        
+        let Address = document.createElement('p');
         let Phone = document.createElement('p');
         let website = document.createElement('p');
 
         h2.textContent = directory[i].name;
         image.setAttribute('src', directory[i].imageurl);
+        Address.textContent = 'Address: ' + directory[i].Address;
         Phone.textContent += 'Phone: ' + directory[i].Phone;
         website.textContent += 'Website: ' + directory[i].website;
         
+        
+
         card.appendChild(h2);
         card.appendChild(image);
+        card.appendChild(Address);
         card.appendChild(Phone);
         card.appendChild(website);
        
 
         document.querySelector('#cards').appendChild(card);
-
-       
         
     }
   });
